19755fe81e93cc11e87630e44fafe9d753951e1f
Uuno Tapper <tapperuuno@gmail.com>
2023-07-22 12:59:37 +0300

    Image puller

---
+++ windows/TmpImagePuller.cmd
@@ -0,0 +1,14 @@
+::Pulls temporary image files from where windows stores them. Such as the default lock screen "Spotlight".
+echo %USERNAME% fetching windows tmp images
+timeout /t 2
+@echo off
+:delf
+echo y | del temp
+:makf
+echo y | rmdir temp /s
+mkdir temp
+xcopy %CD%\Users\%USERNAME%\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets .\temp
+cd .\temp
+ren * *.png
+
+
